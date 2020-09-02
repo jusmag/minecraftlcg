f27e0f2e9e6e1a4b6ce77aa5db487a073cee075d
jusmag <65278847+jusmag@users.noreply.github.com>
2020-09-02 16:35:59 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,28 @@
 function lcg(a,c,m,seed,iter) {
   var s = seed;
-  var output = "";
-  output += s;
   for(var i = 0; i < iter; i++) {
     s = ((a * s + c) % m);
-    output += ",";
-    output += s;
   }
-  console.log(output);
-  return(s);
+  return s;
 }
 
-var a = 25214903917;
-var c = 11;
-var m = Math.pow(2,48);
-lcg(a,c,m,3511347017271779066,11);
+function forwardlcg(seed,iter) {
+  var multiplier = 25214903917;
+  var increment = 11;
+  var period = Math.pow(2,48);
+  return lcg(multiplier,increment,period,seed,iter);
+}
+
+function reverselcg(seed,iter) {
+  var multiplier = 246154705703781;
+  var increment = 107048004364969;
+  var period = Math.pow(2,48);
+  return lcg(multiplier,increment,period,seed,iter);
+}
+
+function run() {
+  console.log(forwardlcg(3,1));
+  console.log(reverselcg(75644711762,1));
+}
+
+run();
