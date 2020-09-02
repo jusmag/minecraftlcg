0f01d87b67485b844f4eca8c038496bad125e168
jusmag <65278847+jusmag@users.noreply.github.com>
2020-09-02 16:39:10 -0700

    added subtract function
    Just made a few minor changes to the add function in order to create a subtract function (this will be useful for division)

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function add(x,y) {
   for(var i = 0; i < stringOne.length + stringTwo.length; i++) { 
     stringAnswer += "0";
   }
-  stringAnswer += x;
+  stringAnswer += stringOne;
   stringAnswer = (stringAnswer.split("")).reverse();
   stringTwo = (stringTwo.split("").reverse());
   for(i = 0; i < stringTwo.length; i++) {
@@ -21,6 +21,38 @@ function add(x,y) {
       var remainder = answer % 10;
       stringAnswer[i] = remainder;
       stringAnswer[i+1] = (parseInt(stringAnswer[i+1])+1).toString();
+    } else {
+      stringAnswer[i] = answer;
+    }
+  }
+  stringAnswer = stringAnswer.reverse().join("");
+  stringAnswer = stringAnswer.replace(/^0+/, '');
+  return(stringAnswer);
+}
+
+function subtract(x,y) {
+  var stringOne = x;
+  var stringTwo = y;
+  if(parseInt(stringTwo) > parseInt(stringOne)) {
+    var temp = stringOne;
+    stringOne = stringTwo;
+    stringTwo = temp;
+  }
+  var stringAnswer = "";
+  for(var i = 0; i < stringOne.length + stringTwo.length; i++) { 
+    stringAnswer += "0";
+  }
+  stringAnswer += stringOne;
+  stringAnswer = (stringAnswer.split("")).reverse();
+  stringTwo = (stringTwo.split("").reverse());
+  for(i = 0; i < stringTwo.length; i++) {
+    var tempOne = parseInt(stringAnswer[i]);
+    var tempTwo = parseInt(stringTwo[i]);
+    var answer = tempOne - tempTwo;
+    if(answer < 0) {
+      var remainder = 10 + answer;
+      stringAnswer[i] = remainder;
+      stringAnswer[i+1] = (parseInt(stringAnswer[i+1])-1).toString();
     } else {
       stringAnswer[i] = answer;
     }
